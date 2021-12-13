c6ad72ce6a53e24575e7d6873e2f57ce9959ba19
Dinesh <dinu1763@gmail.com>
2021-12-13 16:30:24 +0530

    Update movie.model.js

--- src/models/movie.model.js
+++ src/models/movie.model.js
@@ -2,9 +2,9 @@ const { Schema, model } = require('mongoose');
 
 const movieSchema = new Schema({
     name:{type:String, required: true},
-    actors :[{type:String, required: true},],
-    languages : [{type:String, required: true},],
-    directors : [{type:String, required: true},],
+    actors :[{type:String, required: true}],
+    languages : [{type:String, required: true}],
+    directors : [{type:String, required: true}],
     poster_url :{type:String, required: true},
     user:{
         type:Schema.Types.ObjectId,
